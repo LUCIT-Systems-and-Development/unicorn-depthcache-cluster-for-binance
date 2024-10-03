3eb12c76a00a144704397a6785bee3923bff85aa
oliver <oliver@zehentleitner.co>
2024-10-03 19:48:06 +0200

    0.0.51

--- dev/release/lucit-ubdcc-dcn.sh
+++ dev/release/lucit-ubdcc-dcn.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-VERSION="0.0.50"
+VERSION="0.0.51"
 
 echo Deploying UBDCC DepthCacheCluster $VERSION to https://i018oau9.c1.de1.container-registry.ovh.net/harbor/projects/3/repositories
 
